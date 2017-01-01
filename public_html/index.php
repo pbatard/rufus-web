2a8a06983484a14837f61bc1379f6b029efd513b
Pete Batard <pete@akeo.ie>
2017-01-01 15:32:36 +0000

    update copyright year

--- public_html/index.php
+++ public_html/index.php
@@ -428,7 +428,7 @@ case "he":
 			<td>&hellip;</td>
 		</tr></table></p>
 		<div class="footer"><table align="center" dir="<?= $dir;?>" cellspacing="0" cellpadding="0" border="0"><tr>
-			<td>Copyright&nbsp;</td><td>©&nbsp;</td><td>2011-2016&nbsp;</td><td><a href="http://pete.akeo.ie">Pete&nbsp;Batard/Akeo</a></td></tr></table>
+			<td>Copyright&nbsp;</td><td>©&nbsp;</td><td>2011-2017&nbsp;</td><td><a href="http://pete.akeo.ie">Pete&nbsp;Batard/Akeo</a></td></tr></table>
 			<? /* Please insert your language and name here.
 If you want people to be able to e-mail you directly about this translation, you can insert your name with something like:
 <a href="mailto:pete@akeo.ie?Subject=Rufus%20Homepage%20translation">Pete Batard</a> */ $tr = _("English translation by Pete Batard"); if (substr($tr,0,4) != "Engl") echo $tr . "<br/>";?>

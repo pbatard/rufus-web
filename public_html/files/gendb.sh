46a477386bbe13a7204091a6612443ac9811173c
Pete Batard <pete@akeo.ie>
2017-04-03 12:33:17 +0100

    minor updates

--- public_html/files/gendb.sh
+++ public_html/files/gendb.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-find . -not -name "*.txt" -not -name "*.sh" -not -name "*pre*" -type f -print0 | xargs -0 sha256sum | sort | uniq -w 64 | awk -F '' '{ for(i=1; i<=64; i+=2) {printf "0x%s%s, ", $i,$(i+1);}} {print "\/\/ " substr($0,69)}'
+find . -not -name "*.txt" -not -name "*.sh" -not -name "*pre*" -type f -print0 | xargs -0 sha256sum | sort | uniq -w 64 | awk -F '' '{ for(i=1; i<=64; i+=2) {printf "0x%s%s, ", $i,$(i+1);}} {print "// " substr($0,69)}'

8e8287f0fd09470fa64e4a7cc846ba4cf3276ce7
Pete Batard <pete@akeo.ie>
2023-02-03 01:11:53 +0000

    switch Google Analytics to Google Tag

--- index.php
+++ index.php
@@ -157,13 +157,12 @@ document.addEventListener("DOMContentLoaded", function(event)
 		document.getElementById('cookie-notice').style.display = 'block';
 });
 </script>
-<script type="text/javascript">
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-37022602-1', 'auto');
-	ga('send', 'pageview');
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6PCK0CM7G9"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'G-6PCK0CM7G9');
 </script>
 <style type="text/css">
 	:root {
